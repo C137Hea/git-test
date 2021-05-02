c762e0e48a864c6b6ebbe5942a649cc44683446e
C137Hea <62950402+C137Hea@users.noreply.github.com>
2021-05-03 04:53:21 +0800

    Update GitTest.java

--- src/main/java/com/zh/git/GitTest.java
+++ src/main/java/com/zh/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git5 master test");
         System.out.println("hello git5 hot-fix test");
         System.out.println("push test");
+         System.out.println("pull test");
     }
 }
